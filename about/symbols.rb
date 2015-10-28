b1a8848631daab3f4a0ef7e5bfd190f6d219f4c0
Mazen <thypari2010+git@gmail.com>
2015-10-28 13:24:48 +0100

    add test to return all symbols add test to return corresponding string

--- about/symbols.rb
+++ about/symbols.rb
@@ -7,4 +7,15 @@ describe "A Ruby symbol" do
     expect(foo1.object_id).to eq foo2.object_id
     expect(foo3.object_id).not_to eq foo4.object_id
   end
+
+  it "can return all symbols currently in the symbol table" do
+    all = Symbol.all_symbols
+    expect(all.size).to be > 0
+  end
+
+  it "can return the corresponding string" do
+    foo1 = :Hello
+    expect(foo1.id2name).to eq "Hello"
+  end
+
 end
