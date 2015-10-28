0b7336ac1a9c0e231da00643ca2e4dcc264dd4d4
rennersebastian <renner.sebastian@gmail.com>
2015-10-28 18:43:36 +0100

    added test cases for blocks

--- about/blocks_and_yield.rb
+++ about/blocks_and_yield.rb
@@ -1,3 +1,12 @@
 describe "Blocks and yield" do
+	it "can be stored in variables and used on demand" do
+		addition = lambda {|a, b| return a + b }
 
+		expect(addition.call(5, 6)).to eq(11)
+	end
+	
+	it "is a sub-class of Object" do
+		empty_block = lambda { }
+		expect(empty_block.class.superclass).to eq(Object)
+	end
 end
