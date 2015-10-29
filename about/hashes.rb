51dec8891012a5cc5cd73a7d32034e9cfa7cc3a0
Joshua Widmann <joshuawidmann@gmx.de>
2015-10-29 23:59:46 +0100

    one minor bug fix and added a few more tests

--- about/hashes.rb
+++ about/hashes.rb
@@ -9,7 +9,7 @@ describe "A Hash in Ruby" do
 		it "should create a hash using new statement" do
 			my_new_hash = Hash.new
 			
-			expect(myhash.class).to eq Hash
+			expect(my_new_hash.class).to eq Hash
 		end
 		it "should create a hash using the rocket" do
 			my_second_hash = {"key" => 7}
@@ -38,11 +38,20 @@ describe "A Hash in Ruby" do
 		it 'should return false if the key isn´t present in the hash' do
 			expect(myhash.has_value?(:false_key)).to eq false
 		end
+		it 'should return false when none-empty hash is asked whether its empty' do
+			expect(myhash.empty?).to eq false
+		end
 	end
+
 	describe "Deleting" do
 		it 'should delete the key value pair and returns the value from the hash whose key is equal to passed key' do
 			expect(myhash.delete(:key)).to eq "value"
 		end
+		it 'should return nil as return value when the key-value pair that is to be deleted is not found' do
+			expect(myhash.delete("not_existing_key")).to eq nil
+		end
+		it 'should return the optional code block when the key to be deleted is not found' do
+			expect(myhash.delete("z") { |el| "#{el} not found" }).to eq "z not found"
+		end
 	end
-
 end
