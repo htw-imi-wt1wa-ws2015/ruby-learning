71c1e2e0b29772ddaf18146812e60f9fb8e76260
Felix Brix <felix.brix@onlinehome.de>
2015-10-30 13:37:20 +0100

    Refactored arrays specs

--- about/arrays.rb
+++ about/arrays.rb
@@ -3,43 +3,36 @@
 # Tu Tran Thi Ngoc, Christina Gutenberg, Nadine Lüdicke
 
 describe "A Ruby Array" do
+  let(:array) { [1, 'two', 3.0, 4, 5] }
+
   it "can have multiple elements" do
-    a = Array.new(5)
-    a = [1, "two", 3.0, 4, 5]
-    expect(a.length).to eq 5
+    expect(array.length).to eq 5
   end
 
   it "can get filled" do
-    a = Array.new(3, true)
-    expect(a).to eq([true, true, true])
+    expect(Array.new(3, true)).to eq [true, true, true]
   end
 
   it "can return a specific element" do
-    a = [1, 2, 3, 4, 5, 6]
-    expect(a[3]).to eq(4)
-    expect(a[100]).to eq(nil)
-    expect(a[-2]).to eq(5)
+    expect(array[3]).  to eq 4
+    expect(array[100]).to eq nil
+    expect(array[-2]). to eq 4
   end
 
   it "can return the first element" do
-    a = [1, 2, 3, 4, 5, 6]
-    expect(a.first).to eq(1)
+    expect(array.first).to eq 1
   end
 
   it "can return the last element" do
-    a = [1, 2, 3, 4, 5, 6]
-    expect(a.last).to eq(6)
+    expect(array.last).to eq 5
   end
 
   it "can return values at specified indices as an Array" do
-    a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    expect(a.values_at(1,3,5)).to eq([2,4,6])
-    expect(a.values_at(3..6, 2..4)).to eq([4,5,6,7,3,4,5])
+    expect(array.values_at(1, 3, 4)).   to eq ['two', 4, 5]
+    expect(array.values_at(0..2, 3..4)).to eq array
   end
 
   it "can be reversed" do
-    a = ["a", "b", "c", "d"]
-    expect(a.reverse!).to eq (["d", "c", "b", "a"])
+    expect(array.reverse!).to eq [5, 4, 3.0, 'two', 1]
   end
-
 end
